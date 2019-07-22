bdb1500076d7a058172201d23793a5de4237d72f
Victor Jian Choe <vchoe89@gmail.com>
2019-07-22 01:23:56 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,3 @@
-
-def introduction_with_languange(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+def introduction(name)
+  puts "Hi, my name is #{name}."
 end
